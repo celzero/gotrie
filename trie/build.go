9705eeb8bc949825198c21346adb2e2fac005553
Murtaza Aliakbar <murtaza@live.in>
2025-03-13 20:55:52 +0530

    build: mmap large files

--- trie/build.go
+++ trie/build.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"syscall"
 	"unsafe"
 )
 
@@ -15,16 +16,14 @@ var W = 16
 var L1 = 32 * 32
 var L2 = 32
 
-func Build(tdpath, rdpath, bcpath, ftpath string) (ftrie *FrozenTrie, err error) {
-	td16, td8, err := readBinary(tdpath)
+func Build(tdpath, rdpath, bcpath, ftpath string, usemmap bool) (ftrie *FrozenTrie, err error) {
+	td16, td8, err := readBinary(tdpath, usemmap)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-	rd16, rd8, err := readBinary(rdpath)
+	rd16, rd8, err := readBinary(rdpath, usemmap)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
@@ -42,7 +41,6 @@ func Build(tdpath, rdpath, bcpath, ftpath string) (ftrie *FrozenTrie, err error)
 	hstatus := fmt.Sprintf("md5 mismatch: %s <-> %s | %s <-> %s", tdmd5hex, tdmd5, rdmd5hex, rdmd5)
 
 	if tdmd5hex != tdmd5 || rdmd5hex != rdmd5 {
-		fmt.Println(hstatus)
 		err = errors.New(hstatus)
 		return
 	}
@@ -55,17 +53,56 @@ func Build(tdpath, rdpath, bcpath, ftpath string) (ftrie *FrozenTrie, err error)
 	return
 }
 
-func readBinary(path string) (*[]uint16, *[]byte, error) {
-	content, err := os.ReadFile(path)
+// mmapBinary mmaps the binary file and returns the uint16 and byte slices
+func mmapBinary(path string) (*[]uint16, *[]byte, error) {
+	if path == "" {
+		return nil, nil, errors.New("trie: empty path")
+	}
+
+	if !lilbo(false) {
+		return nil, nil, errors.New("trie: mmap on little endian only")
+	}
+
+	// Open the file
+	file, err := os.Open(path)
 	if err != nil {
-		fmt.Println("err read file : build.go -> read_file_u16()")
+		return nil, nil, err
+	}
+	defer file.Close()
+
+	// Get the file size
+	fi, err := file.Stat()
+	if err != nil {
+		return nil, nil, err
+	}
+	size := fi.Size()
+
+	data, err := syscall.Mmap(int(file.Fd()), 0, int(size), syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
 		return nil, nil, err
 	}
 
-	fmt.Printf("read from %s len %d\n", path, len(content))
+	fmt.Println("trie: mmaped", path, "size", len(data))
+
+	return bytesToUint16(&data), &data, nil
+}
+
+func readBinary(path string, usemmap bool) (*[]uint16, *[]byte, error) {
+	isLittleEndian := lilbo(false)
+	if usemmap && isLittleEndian {
+		return mmapBinary(path)
+	}
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("trie: err read file : build.go -> read_file_u16()")
+		return nil, nil, err
+	}
+
+	fmt.Printf("trie: read from %s len %d\n", path, len(content))
 
 	// works only on little endian machines: go.dev/play/p/50t1HxCr9DV
-	if lilbo(false) {
+	if isLittleEndian {
 		return bytesToUint16(&content), &content, nil
 	}
 
@@ -73,8 +110,7 @@ func readBinary(path string) (*[]uint16, *[]byte, error) {
 	tmp16 := make([]uint16, len(content)/2)
 	err = binary.Read(r, binary.LittleEndian, &tmp16)
 	if err != nil {
-		fmt.Println("err byte2uint: build.go -> read_file_u16()")
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("trie: err: %v", err)
 	}
 	return &tmp16, &content, nil
 }
@@ -82,15 +118,13 @@ func readBinary(path string) (*[]uint16, *[]byte, error) {
 func readBasicConfig(filepath string) (map[string]any, error) {
 	data, err := os.ReadFile(filepath)
 	if err != nil {
-		fmt.Print(err)
 		return nil, err
 	}
 
 	var jobj map[string]any
 	err = json.Unmarshal(data, &jobj)
 	if err != nil {
-		fmt.Println("could not read basicconfig:", err)
-		return nil, err
+		return nil, fmt.Errorf("trie: err reading basicconfig: %v", err)
 	}
 
 	return jobj, err
